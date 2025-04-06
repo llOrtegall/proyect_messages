080e0910f753b66cd319f0f4472b74f0dd61a3a4
llOrTegall <IvanOrtega_97@hotmail.com>
2025-04-06 21:51:30 +0000

    agrengado componente para login en front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,8 @@
+import RegisterPage from "@/components/register"
+
 function App() {
 
-  return (
-    <>
-      <h1>Welcome to the React App</h1>
-      <p>This is a simple React application.</p>
-      <p>It is using TypeScript for type safety.</p>
-      <p>Enjoy coding!</p>
-    </>
-  )
+  return <RegisterPage />
 }
 
 export default App
