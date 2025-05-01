9794930542dd9397d1dc41bde8f8198ae4b46e68
llortegall <ivanortega_97@hotmail.com>
2025-05-01 20:40:38 +0000

    add login route on user routes

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -1,8 +1,10 @@
-import { registerUser, userProfile } from "../controller/user";
+import { registerUser, loginUser, userProfile } from "../controller/user";
 import { Router } from "express";
 
 export const usersRouter = Router();
 
 usersRouter.post('/register', registerUser);
+
+usersRouter.post('/login', loginUser);
 
 usersRouter.get('/profile', userProfile);
