2b9c92058ffea0fc2d314c944d000b61d840d654
llortegall <ivanortega_97@hotmail.com>
2026-04-05 10:00:00 -0500

    delete all files

--- client/src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import axios from 'axios';
-import './index.css';
-
-axios.defaults.baseURL = import.meta.env.VITE_API_URL
-axios.defaults.withCredentials = true
-
-createRoot(document.getElementById('root')!).render(<App />)
